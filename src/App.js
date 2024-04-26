425f98e277422e928b851304c3bc005a6d84e1cc
saichethanr <saichethan9019@gmail.com>
2024-04-26 21:27:12 +0530

    navbar updated

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import ExerciseDetail from './pages/ExerciseDetail';
 import  Home  from './pages/Home';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+
 const App = () => {
   return (
     // making the box respomsive
