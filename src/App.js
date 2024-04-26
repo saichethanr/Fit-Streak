71c7e54024bb29a21e0aa33b372975a2a60614c9
saichethanr <saichethan9019@gmail.com>
2024-04-26 17:57:29 +0530

    basic structure set up

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import React from 'react'
-
+import './App.css';
+// this is used for routing between diffrent pages
+import { Route,Routes } from 'react-router-dom';
+import { Box } from '@mui/material';
+import ExerciseDetail from './pages/ExerciseDetail';
+import  Home  from './pages/Home';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 const App = () => {
   return (
-    <div>App</div>
+    // making the box respomsive
+    <Box width="400" sx={{ width : {x1:'1488px'}}} m="auto">
+       <Navbar/>
+       <Routes>
+          <Route path="/" element = {<Home/>}/>
+          <Route path="/exercise/:id" element = {<ExerciseDetail/>}/>
+       </Routes>
+       <Footer/>
+    </Box>
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 const root  = ReactDOM.createRoot(document.getElementById('root'));
 
-
-root.render(<App/>)
+//wrqaping the app with the browser router to rnable the usage odf the routes in the app
+root.render(
+    <BrowserRouter>
+    <App/>
+    </BrowserRouter>
+)
 
 
 
